e30f1385e53a50d81800789241cf353a37572ce9
edwardOWO <a0981202524@gmail.com>
2025-01-20 09:57:36 +0800

    helm example

--- main.go
+++ main.go
@@ -1,97 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net"
+	"os"
 
-	_ "github.com/edwardOWO/goexample/learn"
-	_ "github.com/edwardOWO/goexample/msg"
-	"github.com/edwardOWO/goexample/server"
-	_ "github.com/edwardOWO/goexample/server"
-	"google.golang.org/grpc"
-	_ "google.golang.org/grpc"
-
-	pb "github.com/edwardOWO/goexample/route"
-
-	_ "github.com/edwardOWO/goexample/route"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/kube"
 )
 
 func main() {
+	// 配置 kubeconfig 路徑
+	kubeconfig := `/home/coder/goexample/test.config`
+	releaseName := "haproxy"                // 替換為你想查詢歷史記錄的 Release 名稱
+	namespace := "default"       // 替換為 Release 所在的命名空間
 
-	// 生成一個listener
-	/*
-		listener, err := net.Listen("tcp", "localhost:5000")
-		if err != nil {
-			log.Fatalln("cannot create a listener a the address")
-		}
-		// server
-		grpcServer := grpc.NewServer()
-		pb.RegisterRouteGuideServer(grpcServer, server.DbServer())
+	// 檢查 kubeconfig 文件是否存在
+	if _, err := os.Stat(kubeconfig); os.IsNotExist(err) {
+		log.Fatalf("kubeconfig 文件不存在: %v", kubeconfig)
+	}
 
-		log.Fatalln(grpcServer.Serve(listener))
-	*/
-	println("gRPC server tutorial in Go")
+	// 初始化 Helm 的 CLI 設定
+	settings := cli.New()
+	settings.KubeConfig = kubeconfig
+	settings.SetNamespace(namespace)
 
-	// simple
-	/*
-		listener, err := net.Listen("tcp", ":9000")
-		if err != nil {
-			panic(err)
-		}
-
-		s := grpc.NewServer()
-
-		pb.RegisterGetTimeServer(s, &server.Server{})
-		if err := s.Serve(listener); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	*/
-
-	// stream rpc
-
-	/*
-		listener, err := net.Listen("tcp", ":9000")
-		if err != nil {
-			panic(err)
-		}
-
-		s := grpc.NewServer()
-
-		pb.RegisterStreamServiceServer(s, server.StreamGRPC{})
-		if err := s.Serve(listener); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	*/
-
-	ServerSideStreamGrpc()
-
-}
-func ClientSideStreamGrpc() {
-	listener, err := net.Listen("tcp", ":9000")
+	// 初始化 Action Configuration
+	actionConfig := new(action.Configuration)
+	clientGetter := kube.GetConfig(settings.KubeConfig, "", settings.Namespace())
+	err := actionConfig.Init(clientGetter, settings.Namespace(), os.Getenv("HELM_DRIVER"), log.Printf)
 	if err != nil {
-		panic(err)
+		log.Fatalf("無法初始化 Helm 設定: %v", err)
 	}
 
-	s := grpc.NewServer()
+	// 創建 History 動作
+	historyClient := action.NewHistory(actionConfig)
 
-	pb.RegisterClientSideServer(s, &server.ClientSideService{})
-
-	//pb.RegisterStreamClientServer(s, server.StreamClient{})
-	if err := s.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-func ServerSideStreamGrpc() {
-	listener, err := net.Listen("tcp", ":9000")
+	// 獲取 Release 的歷史記錄
+	histories, err := historyClient.Run(releaseName)
 	if err != nil {
-		panic(err)
+		log.Fatalf("無法取得 Release 的歷史記錄: %v", err)
 	}
 
-	s := grpc.NewServer()
-
-	pb.RegisterServerSideServer(s, &server.ServerSideService{})
-
-	if err := s.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	// 輸出歷史記錄
+	fmt.Printf("Release '%s' 的歷史記錄:\n", releaseName)
+	if len(histories) == 0 {
+		fmt.Println("無歷史記錄")
+	} else {
+		for _, history := range histories {
+			fmt.Printf("- 修訂版本: %d, 狀態: %s, 更新時間: %s\n",
+				history.Version,
+				history.Info.Status,
+				history.Info.LastDeployed,
+			)
+		}
 	}
 }
